ae3045fff5ecf5c5e5731abb29c320d2079b95ec
Valentinas B <valentui@gmail.com>
2018-02-26 19:28:28 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+app-code-search
+===============
+
+A Symfony project created on February 26, 2018, 4:45 pm.
---
+++ src/Vb/Bundle/CodeSearchBundle/VbCodeSearchBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Vb\Bundle\CodeSearchBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class VbCodeSearchBundle extends Bundle
+{
+}
